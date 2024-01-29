7e8977f10edc11eea24a46b99de32db4e7a24e0e
dima.avdeev <dima.avdeev@jetbrains.com>
2024-01-29 21:12:29 +0400

    issue https://github.com/JetBrains/compose-multiplatform/issues/4153

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,16 +1,12 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "KotlinProject") {
-        App()
+    Window(onCloseRequest = ::exitApplication, title = "Window", undecorated = true, transparent = true) {
+        Surface(color = Color.Green, modifier = Modifier.fillMaxSize()) { }
     }
-}
-
-@Preview
-@Composable
-fun AppDesktopPreview() {
-    App()
 }
